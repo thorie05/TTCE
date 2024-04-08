202e55d170f87bd5da86751fe89d268484ca92b9
Thomas Riedle <tom.riedle@gmail.com>
2024-04-08 13:06:46 +0200

    changed folder structure

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O0 -Wall -shared -std=c++23 -fPIC $(python3 -m pybind11 --includes) $(find engine -type f -name "*.cpp") -o game/ttce$(python3-config --extension-suffix)
+g++ -O0 -Wall -shared -std=c++23 -fPIC $(python3 -m pybind11 --includes) $(find engine -type f -not -path "engine/magic/*" -name "*.cpp") -o game/ttce$(python3-config --extension-suffix)

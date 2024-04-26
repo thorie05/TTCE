70fdae7a60fd224f3b1a99ae86b36697045859cc
Thomas Riedle <tom.riedle@gmail.com>
2024-04-26 23:21:38 +0200

    started creating lookup arrays for leaping pieces

---
+++ auxiliary/leaping/generate_move_masks.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef unsigned long long U64;
+
+int main() {
+    array<U64, 64> knightMoveMasks;
+
+    for (int sq = 0; sq < 64; sq++) {
+        U64 mask = 0ULL;
+
+    }
+
+    return 0;
+}

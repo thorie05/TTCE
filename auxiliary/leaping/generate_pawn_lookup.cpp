1df082c8e516c515dd1a8447f95ef06e73ec3f81
Thomas Riedle <tom.riedle@gmail.com>
2024-05-05 10:11:43 +0200

    improved black pawn move mask generation

--- auxiliary/leaping/generate_pawn_lookup.cpp
+++ auxiliary/leaping/generate_pawn_lookup.cpp
@@ -34,10 +34,17 @@ int main() {
         whitePawnMasks[sq] = mask;
     }
 
+    // mask for white:
+    // first bit: left capture square
+    // second bit: push square
+    // third bit: right capture square
+    // fourth bit: double push square
+
     // white pawn lookup array
     for (int sq = 0; sq < 56; sq ++) {
         for (int i = 0; i < 16; i++) {
             U64 mask = 0ULL;
+
             if (!(i & 2)) {
                 mask |= 1ULL << (sq + 8);
                 if (sq / 8 == 1 && !(i & 8)) {
@@ -50,6 +57,7 @@ int main() {
             if (i & 4) {
                 mask |= 1ULL << (sq + 9);
             }
+
             whitePawnLookup[sq * 16 + i] = mask;
         }
     }
@@ -77,33 +85,30 @@ int main() {
         blackPawnMasks[sq] = mask;
     }
 
+    // mask for black:
+    // first bit: double push square
+    // second bit: right capture square
+    // third bit: push square
+    // fourth bit: left capture square
+
     // black pawn lookup array
     for (int sq = 8; sq < 64; sq ++) {
         for (int i = 0; i < 16; i++) {
             U64 mask = 0ULL;
-            // square 8 is special to avoid negative bit shift
-            if (sq == 8) {
-                if (!(i & 1)) {
-                    mask |= 1ULL;
-                }
-                if (i & 2) {
-                    mask |= 2ULL;
+
+            if (!(i & 4)) {
+                mask |= 1ULL << (sq - 8);
+                if (sq / 8 == 6 && !(i & 1)) {
+                    mask |= 1ULL << (sq - 16);
                 }
             }
-            else {
-                if (!(i & 2)) {
-                    mask |= 1ULL << (sq - 8);
-                    if (sq / 8 == 6 && !(i & 8)) {
-                        mask |= 1ULL << (sq - 16);
-                    }
-                }
-                if (i & 1) {
-                    mask |= 1ULL << (sq - 9);
-                }
-                if (i & 4) {
-                    mask |= 1ULL << (sq - 7);
-                }
+            if (i & 2) {
+                mask |= 1ULL << (sq - 9);
             }
+            if (i & 8) {
+                mask |= 1ULL << (sq - 7);
+            }
+
             blackPawnLookup[sq * 16 + i] = mask;
         }
     }
--- engine/intern/get_black_pawn_move_mask.cpp
+++ engine/intern/get_black_pawn_move_mask.cpp
@@ -6,10 +6,10 @@ U64 Chessboard::getBlackPawnMoveMask(const int square) {
     returns move mask for a black pawn on the given square
     */
 
-    U64 key = (bitboards[PIECES] & BLACK_PAWN_MASK[square]);
-    int doublePushPos = std::max(0, square - 16);
-    key >>= std::max(0, square - 9);
-    key |= ((key & 1ULL << doublePushPos) >> doublePushPos) << 3;
+    U64 key = (bitboards[PIECES] & BLACK_PAWN_MASK[square]) << (70 - square);
+    U64 doublePushBit = key & 1ULL << 54;
+    key ^= doublePushBit | (doublePushBit << 6);
+    key >>= 60;
 
     return BLACK_PAWN_LOOKUP[square * 16 + key];
 }
